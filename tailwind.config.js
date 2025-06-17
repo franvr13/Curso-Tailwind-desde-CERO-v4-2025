8d2ae934c3b36f8e0bf8e75f080728d6f7515f5a
franvr13 <f13villalbaroldan@gmail.com>
2025-06-17 15:41:53 +0200

    Añadido un nuevo certificado.

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html",
+    "./assets/**/*.{js,ts,jsx,tsx,html}",
+  ],
+  theme: {
+    extend: {
+    },
+  },
+  plugins: [],
+}
